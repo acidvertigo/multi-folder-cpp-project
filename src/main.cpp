41d79333b2a7f51fc80e9ac0a590c0a7e1df0c37
Luca <ldanzelmo@gmail.com>
2018-02-06 13:56:48 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 using namespace Foo;
 
 int main() {
-  Sample sm(44100);
+  Shared sm(44100);
   cout << "Hello World!" << endl;
   cout << "Sample value is: " << sm.getSam() << endl;
   return 0;
